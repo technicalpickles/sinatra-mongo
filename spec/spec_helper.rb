7e36d13e50ce80b3783ba51636181e624b4ae0c3
Harold Giménez <harold.gimenez@gmail.com>
2009-12-10 18:38:10 -0500

    Require the right path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'sinatra-mongo'
+require 'sinatra/mongo'
 require 'spec'
 require 'spec/autorun'
 
