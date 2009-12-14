7ef34b4f57f02858b1e72102724202cdc90d1e37
Harold Giménez <harold.gimenez@gmail.com>
2009-12-14 11:47:03 -0500

    Refactored repetition by creating a shared example group for sinatra apps

--- spec/sinatra-mongo_modular_style_spec.rb
+++ spec/sinatra-mongo_modular_style_spec.rb
@@ -8,21 +8,6 @@ describe 'a modular style sinatra app' do
     ModularApp
   end
 
-  describe 'GET /' do
-    before(:each) do
-      3.times { |i| mongo['test_collection'].insert({'x' => i }) }
-    end
-
-    after(:each) do
-      mongo['test_collection'].remove
-    end
-
-    it 'fetches and shows the first element in test_collection' do
-      get '/' do |response|
-        response.body.should match('"x"=>0')
-      end
-    end
-
-  end
+  it_should_behave_like 'all sinatra-mongo test apps'
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,21 @@ require 'rack/test'
 Spec::Runner.configure do |config|
   
 end
+
+shared_examples_for 'all sinatra-mongo test apps' do
+  describe 'GET /' do
+    before(:each) do
+      3.times { |i| mongo['test_collection'].insert({'x' => i }) }
+    end
+
+    after(:each) do
+      mongo['test_collection'].remove
+    end
+
+    it 'fetches and shows the first element in test_collection' do
+      get '/' do |response|
+        response.body.should match('"x"=>0')
+      end
+    end
+  end
+end
