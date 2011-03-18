02f0c81912189018a024ab98acc03d039c61ba43
LI Daobing <lidaobing@gmail.com>
2011-03-18 17:46:43 +0800

    support mongo replica set

--- lib/sinatra/mongo.rb
+++ lib/sinatra/mongo.rb
@@ -12,6 +12,14 @@ module Sinatra
     def mongo=(url)
       @mongo = nil
       set :mongo_url, url
+      @mongo_replicaset_config = nil
+      mongo
+    end
+
+    def mongo_replicaset_config=(hash)
+      @mongo = nil
+      set :mongo_url, nil
+      @mongo_replicaset_config = hash
       mongo
     end
 
@@ -20,11 +28,28 @@ module Sinatra
       @mongo_lock ||= Mutex.new
       @mongo_lock.synchronize do
         @mongo ||= (
-          url = URI(mongo_url)
-          connection = Mongo::Connection.new(url.host, url.port)
-          mongo = connection.db(url.path[1..-1], mongo_settings)
-          if url.user && url.password
-            mongo.authenticate(url.user, url.password)
+          if mongo_url
+            url = URI(mongo_url)
+            connection = Mongo::Connection.new(url.host, url.port)
+            mongo = connection.db(url.path[1..-1], mongo_settings)
+            if url.user && url.password
+              mongo.authenticate(url.user, url.password)
+            end
+          elsif @mongo_replicaset_config
+            hosts = @mongo_replicaset_config[:hosts]
+            database = @mongo_replicaset_config[:database]
+            read_secondary = @mongo_replicaset_config[:read_secondary] || false
+            raise "hosts required" if hosts.nil?
+            raise "database required" if database.nil?
+            connection = ReplSetConnection.new(hosts, :read_secondary => read_secondary)
+            mongo = connection.db(database, mongo_settings)
+            username = @mongo_replicaset_config[:username]
+            password = @mongo_replicaset_config[:password]
+            if username && password
+              mongo.authenticate(username, password)
+            end
+          else
+            mongo = nil
           end
           mongo
         )
